a1889d1228556132d7fdefec4907feb0d86bd5a5
Pieter-Jan Hoedt <hoedt@ml.jku.at>
2021-01-14 11:59:34 +0100

    make recurrent functions runnable

--- experiments/simple_function_recurrent.py
+++ experiments/simple_function_recurrent.py
@@ -20,64 +20,165 @@ parser.add_argument('--operation',
                     ],
                     type=str,
                     help='Specify the operation to use, e.g. add, mul, squared')
+parser.add_argument('--input-size',
+                    action='store',
+                    default=100,
+                    type=int,
+                    help='Specify the input size')
+parser.add_argument('--num-subsets',
+                    action='store',
+                    default=2,
+                    type=int,
+                    help='Specify the number of subsets to use')
+parser.add_argument('--subset-ratio',
+                    action='store',
+                    default=0.25,
+                    type=float,
+                    help='Specify the subset-size as a fraction of the input-size')
+parser.add_argument('--overlap-ratio',
+                    action='store',
+                    default=0.5,
+                    type=float,
+                    help='Specify the overlap-size as a fraction of the input-size')
+parser.add_argument('--simple',
+                    action='store_true',
+                    default=False,
+                    help='Use a very simple dataset with t = sum(v[0:2]) + sum(v[4:6])')
+
 parser.add_argument('--seed',
                     action='store',
                     default=0,
                     type=int,
                     help='Specify the seed to use')
+parser.add_argument('--batch-size',
+                    action='store',
+                    default=128,
+                    type=int,
+                    help='Specify the batch-size to be used for training')
 parser.add_argument('--max-iterations',
                     action='store',
                     default=100000,
                     type=int,
                     help='Specify the max number of iterations to use')
-parser.add_argument('--cuda',
-                    action='store',
-                    default=torch.cuda.is_available(),
-                    type=bool,
-                    help=f'Should CUDA be used (detected automatically as {torch.cuda.is_available()})')
-parser.add_argument('--simple',
+
+parser.add_argument('--no-cuda',
                     action='store_true',
                     default=False,
-                    help='Use a very simple dataset with t = sum(v[0:2]) + sum(v[4:6])')
+                    help=f'Force no CUDA (cuda usage is detected automatically as {torch.cuda.is_available()})')
+parser.add_argument('--name-prefix',
+                    action='store',
+                    default='simple_function_static',
+                    type=str,
+                    help='Where the data should be stored')
+parser.add_argument('--remove-existing-data',
+                    action='store_true',
+                    default=False,
+                    help='Should old results be removed')
 parser.add_argument('--verbose',
                     action='store_true',
                     default=False,
                     help='Should network measures (e.g. gates) and gradients be shown')
+
+parser.add_argument('--regualizer',
+                    action='store',
+                    default=10,
+                    type=float,
+                    help='Specify the regualization lambda to be used')
+parser.add_argument('--regualizer-z',
+                    action='store',
+                    default=0,
+                    type=float,
+                    help='Specify the z-regualization lambda to be used')
+parser.add_argument('--regualizer-oob',
+                    action='store',
+                    default=1,
+                    type=float,
+                    help='Specify the oob-regualization lambda to be used')
+parser.add_argument('--regualizer-scaling',
+                    action='store',
+                    default='linear',
+                    choices=['exp', 'linear'],
+                    type=str,
+                    help='Use an expoentational scaling from 0 to 1, or a linear scaling.')
+parser.add_argument('--regualizer-scaling-start',
+                    action='store',
+                    default=1000000,
+                    type=int,
+                    help='Start linear scaling at this global step.')
+parser.add_argument('--regualizer-scaling-end',
+                    action='store',
+                    default=2000000,
+                    type=int,
+                    help='Stop linear scaling at this global step.')
 args = parser.parse_args()
+
+
+setattr(args, 'cuda', torch.cuda.is_available() and not args.no_cuda)
 
 # Print configuration
 print(f'running')
-print(f'  - seed: {args.seed}')
-print(f'  - operation: {args.operation}')
 print(f'  - layer_type: {args.layer_type}')
-print(f'  - simple: {args.simple}')
-print(f'  - cuda: {args.cuda}')
-print(f'  - verbose: {args.verbose}')
+print(f'  - operation: {args.operation}')
+print(f'  - regualizer: {args.regualizer}')
+print(f'  - regualizer_z: {args.regualizer_z}')
+print(f'  - regualizer_oob: {args.regualizer_oob}')
+print(f'  -')
 print(f'  - max_iterations: {args.max_iterations}')
+print(f'  - batch_size: {args.batch_size}')
+print(f'  - seed: {args.seed}')
+print(f'  -')
+print(f'  - input_size: {args.input_size}')
+print(f'  - num_subsets: {args.num_subsets}')
+print(f'  - subset_ratio: {args.subset_ratio}')
+print(f'  - overlap_ratio: {args.overlap_ratio}')
+print(f'  - simple: {args.simple}')
+print(f'  -')
+print(f'  - cuda: {args.cuda}')
+print(f'  - name_prefix: {args.name_prefix}')
+print(f'  - remove_existing_data: {args.remove_existing_data}')
+print(f'  - verbose: {args.verbose}')
 
 # Prepear logging
 # results_writer = stable_nalu.writer.ResultsWriter('simple_function_recurrent')
 summary_writer = stable_nalu.writer.SummaryWriter(
-    f'simple_function_recurrent/{args.layer_type.lower()}_{args.operation.lower()}_{args.seed}'
+    f'{args.name_prefix}/{args.layer_type.lower()}'
+    f'_op-{args.operation.lower()}'
+    f'_rs-{args.regualizer_scaling}'
+    f'_rl-{args.regualizer_scaling_start}-{args.regualizer_scaling_end}'
+    f'_r-{args.regualizer}-{args.regualizer_z}-{args.regualizer_oob}'
+    f'_z-{"simple" if args.simple else f"{args.input_size}-{args.subset_ratio}-{args.overlap_ratio}"}'
+    f'_z{args.num_subsets}'
+    f'_b{args.batch_size}'
+    f'_s{args.seed}',
+    remove_existing_data=args.remove_existing_data
 )
 
 # Set seed
 torch.manual_seed(args.seed)
+torch.backends.cudnn.deterministic = True
 
 # Setup datasets
 dataset = stable_nalu.dataset.SimpleFunctionRecurrentDataset(
     operation=args.operation,
+    input_size=args.input_size,
+    subset_ratio=args.subset_ratio,
+    overlap_ratio=args.overlap_ratio,
+    num_subsets=args.num_subsets,
     simple=args.simple,
     use_cuda=args.cuda,
     seed=args.seed
 )
-dataset_train = iter(dataset.fork(seq_length=10).dataloader(batch_size=128))
-dataset_valid_interpolation = iter(dataset.fork(seq_length=10).dataloader(batch_size=2048))
-dataset_valid_extrapolation = iter(dataset.fork(seq_length=1000).dataloader(batch_size=2048))
+print(f'  -')
+print(f'  - dataset: {dataset.print_operation()}')
+
+dataset_train = iter(dataset.fork(seq_length=10).dataloader(batch_size=args.batch_size))
+dataset_valid_interpolation = iter(dataset.fork(seq_length=10, seed=43953907).dataloader(batch_size=1000))
+dataset_valid_extrapolation = iter(dataset.fork(seq_length=1000, seed=8689336).dataloader(batch_size=1000))
 
 # setup model
 model = stable_nalu.network.SimpleFunctionRecurrentNetwork(
     args.layer_type,
+    input_size=dataset.get_input_size(),
     writer=summary_writer.every(1000) if args.verbose else None
 )
 if args.cuda:
@@ -101,13 +202,26 @@ for epoch_i, (x_train, t_train) in zip(range(args.max_iterations + 1), dataset_t
 
     # Log validation
     if epoch_i % 1000 == 0:
-        summary_writer.add_scalar('loss/valid/interpolation', test_model(dataset_valid_interpolation))
-        summary_writer.add_scalar('loss/valid/extrapolation', test_model(dataset_valid_extrapolation))
+        interpolation_error = test_model(dataset_valid_interpolation)
+        extrapolation_error = test_model(dataset_valid_extrapolation)
+
+        summary_writer.add_scalar('metric/valid/interpolation', interpolation_error)
+        summary_writer.add_scalar('metric/test/extrapolation', extrapolation_error)
 
     # forward
     y_train = model(x_train)
+    regualizers = model.regualizer()
+
+    if (args.regualizer_scaling == 'linear'):
+        r_w_scale = max(0, min(1, (
+            (epoch_i - args.regualizer_scaling_start) /
+            (args.regualizer_scaling_end - args.regualizer_scaling_start)
+        )))
+    elif (args.regualizer_scaling == 'exp'):
+        r_w_scale = 1 - math.exp(-1e-5 * epoch_i)
+
     loss_train_criterion = criterion(y_train, t_train)
-    loss_train_regualizer = 0.1 * (1 - math.exp(-1e-5 * epoch_i)) * model.regualizer()
+    loss_train_regualizer = args.regualizer * r_w_scale * regualizers['W'] + regualizers['g'] + args.regualizer_z * regualizers['z'] + args.regualizer_oob * regualizers['W-OOB']
     loss_train = loss_train_criterion + loss_train_regualizer
 
     # Log loss
@@ -115,7 +229,7 @@ for epoch_i, (x_train, t_train) in zip(range(args.max_iterations + 1), dataset_t
     summary_writer.add_scalar('loss/train/regualizer', loss_train_regualizer)
     summary_writer.add_scalar('loss/train/total', loss_train)
     if epoch_i % 1000 == 0:
-        print(f'train {epoch_i}: {loss_train_criterion}')
+        print('train %d: %.5f, inter: %.5f, extra: %.5f' % (epoch_i, loss_train_criterion, interpolation_error, extrapolation_error))
 
     # Optimize model
     if loss_train.requires_grad:
@@ -137,16 +251,5 @@ print(f'  - loss_train: {loss_train}')
 print(f'  - loss_valid_inter: {loss_valid_inter}')
 print(f'  - loss_valid_extra: {loss_valid_extra}')
 
-# save results
-#results_writer.add({
-#    'seed': args.seed,
-#    'operation': args.operation,
-#    'layer_type': args.layer_type,
-#    'simple': args.simple,
-#    'cuda': args.cuda,
-#    'verbose': args.verbose,
-#    'max_iterations': args.max_iterations,
-#    'loss_train': loss_train,
-#    'loss_valid_inter': loss_valid_inter,
-#    'loss_valid_extra': loss_valid_extra
-#})
+# Use saved weights to visualize the intermediate values.
+stable_nalu.writer.save_model(summary_writer.name, model)
--- stable_nalu/dataset/simple_function_recurrent.py
+++ stable_nalu/dataset/simple_function_recurrent.py
@@ -2,18 +2,8 @@
 from ._simple_function_abstact import SimpleFunctionDataset
 
 class SimpleFunctionRecurrentDataset(SimpleFunctionDataset):
-    def __init__(self, operation,
-                 vector_size=10,
-                 min_subset_length=1,
-                 max_subset_length=5,
-                 min_subset_overlap=1,
-                 max_subset_overlap=2, **kwargs):
-        super().__init__(operation,
-                         vector_size=vector_size,
-                         min_subset_length=min_subset_length,
-                         max_subset_length=max_subset_length,
-                         min_subset_overlap=min_subset_overlap,
-                         max_subset_overlap=max_subset_overlap, **kwargs)
+    def __init__(self, operation, input_size=10, **kwargs):
+        super().__init__(operation, input_size=input_size, **kwargs)
 
-    def fork(self, seq_length=10, input_range=1, *args, **kwargs):
-        return super().fork((seq_length, self._vector_size), input_range, *args, **kwargs)
+    def fork(self, seq_length=10, input_range=[1, 2], *args, **kwargs):
+        return super().fork((seq_length, self._input_size), input_range, *args, **kwargs)
